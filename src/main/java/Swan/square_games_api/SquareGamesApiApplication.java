d4c505de89a19ee58dd7abd6e00b87bb928f2c79
v-swan <swan.vuillet@hotmail.com>
2026-09-10 11:23:35 +0200

    initial commit

---
+++ src/main/java/Swan/square_games_api/SquareGamesApiApplication.java
@@ -0,0 +1,13 @@
+package Swan.square_games_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquareGamesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SquareGamesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Swan/square_games_api/SquareGamesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package Swan.square_games_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquareGamesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
